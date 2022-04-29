f9d77de42dbd48e6621cc8be3f37d3cec9044808
cloyd <abadcloyd@gmail.com>
2022-04-29 23:32:04 +0800

    made landing page more responsive

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <Component {...pageProps} />
+    </div>
+  )
+
 }
 
 export default MyApp
