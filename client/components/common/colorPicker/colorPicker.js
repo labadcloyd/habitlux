e6f5b641fbfb447ef0ea3e8000c8483503033a29
cloyd <abadcloyd@gmail.com>
2022-05-27 14:07:50 +0800

    minor changes to the animations

--- client/components/common/colorPicker/colorPicker.js
+++ client/components/common/colorPicker/colorPicker.js
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion } from "framer-motion"
+
 import { DEFAULT_COLOR_LIST } from '../../../common/constants/defaults'
 import { useOutsideAlerter } from '../../../common/utils'
 import css from './colorPicker.module.css'
@@ -30,28 +32,44 @@ export default function ColorPicker(props) {
 	},[value])
 
 	return (
-		<div className={css.wrapper}>
-			<div className={css.titleWrapper}>
-				<div 
+		<motion.div className={css.wrapper}>
+			<motion.div className={css.titleWrapper}>
+				<motion.div
+					whileHover={{ scale:1.1, boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.2)' }}
+					whileTap={{ scale:0.9, boxShadow: '0px 0px 0px rgba(0, 0, 0, 0.5)' }}
+
 					className={css.colorContainer} 
 					style={{backgroundColor: `rgb(${currentColor})`}} 
 					onClick={() => (setIsOpen(true))}
 				/>
-				<div className={css.titleContainer}>{children}</div>
-			</div>
+				<motion.div className={css.titleContainer}>{children}</motion.div>
+			</motion.div>
 
 			{isOpen &&
-				<div className={css.colorPickerWrapper} ref={colorPickerRef}>
-					{DEFAULT_COLOR_LIST.map((color, i) => (
-						<div
-							key={i}
-							className={css.colorContainer} 
-							style={{backgroundColor: `rgb(${color})`}}
-							onClick={() => handleChange(color)}						
-						/>
-					))}
-				</div>
+				<AnimatePresence>
+					<motion.div 
+						initial={{opacity: 1, scale: 0}}
+						animate={{opacity: 1, scale: 1}}
+						transition={{ type: 'spring', duration: 0.3 }}
+						exit={{opacity: 1, scale: 0}}
+						layout
+					
+						className={css.colorPickerWrapper} ref={colorPickerRef}
+					>
+						{DEFAULT_COLOR_LIST.map((color, i) => (
+							<motion.div
+								whileHover={{ scale:1.1, boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.2)' }}
+								whileTap={{ scale:0.9, boxShadow: '0px 0px 0px rgba(0, 0, 0, 0.5)' }}
+								
+								key={i}
+								className={css.colorContainer} 
+								style={{backgroundColor: `rgb(${color})`}}
+								onClick={() => handleChange(color)}						
+							/>
+						))}
+					</motion.div>
+				</AnimatePresence>
 			}
-		</div>
+		</motion.div>
 	)
 }
