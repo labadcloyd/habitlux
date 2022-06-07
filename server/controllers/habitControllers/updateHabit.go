4762d70f82d517393c0c0089c9b5bdff5ff726b7
cloyd <abadcloyd@gmail.com>
2022-06-07 20:11:16 +0800

    updated updatehabit and updatehabitlist route

--- server/controllers/habitControllers/updateHabit.go
+++ server/controllers/habitControllers/updateHabit.go
@@ -1,82 +1,75 @@
 package controllers
 
-// import (
-// 	"habit-tracker/database"
-// 	"habit-tracker/helpers"
-// 	"habit-tracker/middlewares"
-// 	"habit-tracker/models"
-// 	"log"
-// 	"strconv"
+import (
+	"habit-tracker/database"
+	"habit-tracker/helpers"
+	"habit-tracker/middlewares"
+	"habit-tracker/models"
+	"log"
+	"strconv"
 
-// 	"github.com/gofiber/fiber/v2"
-// 	"github.com/golang-jwt/jwt/v4"
-// )
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
 
 
-// func UpdateHabit(c *fiber.Ctx) error {
-// 	//* auth middleware
-// 	token := middlewares.AuthMiddleware(c)
-// 	if token == nil {
-// 		return c.JSON(fiber.Map{
-// 			"message": "Unauthenticated",
-// 		})
-// 	}
-// 	claims := token.Claims.(*jwt.RegisteredClaims)
-// 	u64, err := strconv.ParseUint(claims.Issuer, 10, 32)
-// 	if err != nil {
-// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	owner_id := uint(u64)
+func UpdateHabit(c *fiber.Ctx) error {
+	//* auth middleware
+	token := middlewares.AuthMiddleware(c)
+	if token == nil {
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*jwt.RegisteredClaims)
+	u64, err := strconv.ParseUint(claims.Issuer, 10, 32)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	owner_id := uint(u64)
 
-// 	//* data validation
-// 	reqData := ReqUpdateHabit{}
-// 	if err := c.BodyParser(&reqData); err != nil {
-// 		log.Println("err: ", err)
-// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	errors := helpers.ValidateStruct(reqData)
-// 	if errors != nil {
-// 		return c.Status(fiber.StatusBadRequest).JSON(errors)
-// 	}
+	//* data validation
+	reqData := ReqUpdateHabit{}
+	if err := c.BodyParser(&reqData); err != nil {
+		log.Println("err: ", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	errors := helpers.ValidateStruct(reqData)
+	if errors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(errors)
+	}
 
-// 	//* updating the habit
-// 	habit := models.Habit {
-// 		ID:										reqData.ID,
-// 		Owner_ID: 						owner_id,
-// 		Habit_Name:						reqData.Habit_Name,
-// 		Date_Created: 				reqData.Date_Created,
-// 		Comment: 							reqData.Comment,
-// 		Target_Repeat_Count: 	reqData.Target_Repeat_Count,
-// 		Repeat_Count: 				reqData.Repeat_Count,
-// 	}
-// 	if err := database.DB.
-// 		Raw(`UPDATE habits
-// 			SET id = ?,
-// 				owner_id = ?,
-// 				habit_name = ?,
-// 				date_created = ?,
-// 				comment = ?,
-// 				target_repeat_count = ?,
-// 				repeat_count = ?
-// 			WHERE owner_id = ? AND id = ?
-// 		`,
-// 		reqData.ID,
-// 		owner_id,
-// 		reqData.Habit_Name,
-// 		reqData.Date_Created,
-// 		reqData.Comment,
-// 		reqData.Target_Repeat_Count,
-// 		reqData.Repeat_Count,
-// 		owner_id, reqData.ID).Scan(&habit).Error; err != nil {
-// 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 				"message": err.Error(),
-// 			})
-// 	}
+	//* updating the habit
+	habit := models.Habit {
+		ID:										reqData.ID,
+		Owner_ID: 						owner_id,
+		Habit_Name:						reqData.Habit_Name,
+		Date_Created: 				reqData.Date_Created,
+		Comment: 							reqData.Comment,
+		Target_Repeat_Count: 	reqData.Target_Repeat_Count,
+		Repeat_Count: 				reqData.Repeat_Count,
+	}
+	if _, err := database.DB.
+		Exec(`UPDATE habits
+			SET
+				habit_name = $1, date_created = $2, comment = $3, target_repeat_count = $4, repeat_count = $5
+			WHERE owner_id = $6 AND id = $7`,
+			reqData.Habit_Name,
+			reqData.Date_Created,
+			reqData.Comment,
+			reqData.Target_Repeat_Count,
+			reqData.Repeat_Count,
+			owner_id, reqData.ID)
+		err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+	}
 
-// 	log.Println("Successfully updated habbit")
-// 	return c.Status(fiber.StatusOK).JSON(habit)
-// }
+	log.Println("Successfully updated habbit")
+	return c.Status(fiber.StatusOK).JSON(habit)
+}
