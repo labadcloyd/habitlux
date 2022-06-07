c4d942c7b2aaf3b15e748a8890b2bcf325aed29c
cloyd <abadcloyd@gmail.com>
2022-06-07 13:55:06 +0800

    rewrote create habits and habitlists routes

--- server/controllers/habitControllers/createHabit.go
+++ server/controllers/habitControllers/createHabit.go
@@ -1,85 +1,109 @@
 package controllers
 
-// import (
-// 	"errors"
-// 	"habit-tracker/database"
-// 	"habit-tracker/helpers"
-// 	"habit-tracker/middlewares"
-// 	"habit-tracker/models"
-// 	"log"
-// 	"strconv"
+import (
+	"database/sql"
+	"habit-tracker/database"
+	"habit-tracker/helpers"
 
-// 	"github.com/gofiber/fiber/v2"
-// 	"github.com/golang-jwt/jwt/v4"
-// 	"gorm.io/gorm"
-// )
+	"habit-tracker/middlewares"
+	"habit-tracker/models"
+	"log"
+
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/lib/pq"
+	"github.com/golang-jwt/jwt/v4"
+)
 
 
-// func CreateHabit(c *fiber.Ctx) error {
-// 	//* auth middleware
-// 	token := middlewares.AuthMiddleware(c)
-// 	if token == nil {
-// 		return c.JSON(fiber.Map{
-// 			"message": "Unauthenticated",
-// 		})
-// 	}
-// 	claims := token.Claims.(*jwt.RegisteredClaims)
-// 	u64, err := strconv.ParseUint(claims.Issuer, 10, 32)
-// 	if err != nil {
-// 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	owner_id := uint(u64)
+func CreateHabit(c *fiber.Ctx) error {
+	//* auth middleware
+	token := middlewares.AuthMiddleware(c)
+	if token == nil {
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*jwt.RegisteredClaims)
+	u64, err := strconv.ParseUint(claims.Issuer, 10, 32)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	owner_id := uint(u64)
+	
+	//* data validation
+	reqData := new(ReqCreateHabit)
+	if err := c.BodyParser(&reqData); err != nil {
+		log.Println("err: ", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	reqErrors := helpers.ValidateStruct(*reqData)
+	if reqErrors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(reqErrors)
+	}
 
-// 	//* data validation
-// 	reqData := new(ReqCreateHabit)
-// 	if err := c.BodyParser(&reqData); err != nil {
-// 		log.Println("err: ", err)
-// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	reqErrors := helpers.ValidateStruct(*reqData)
-// 	if reqErrors != nil {
-// 		return c.Status(fiber.StatusBadRequest).JSON(reqErrors)
-// 	}
+	//* checking if record exists
+	oldHabit := models.Habit{}
+	row := database.DB.
+		QueryRow(`
+			SELECT id FROM habits 
+			WHERE owner_id = $1 AND date_created = $2 AND habit_name = $3`,
+			owner_id, reqData.Date_Created, reqData.Habit_Name,
+		)
+	err = row.Scan(&oldHabit.ID)
+	// only checking if the error is not caused by empty rows
+	if err != nil && err != sql.ErrNoRows {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	// returning error if record exists
+	if (oldHabit != models.Habit{}) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Habit already exists",
+		})
+	}
 
-// 	//* checking if record exists
-// 	oldHabit := models.Habit{}
+	//* saving the habit
+	habit := models.Habit {
+		Owner_ID: 						owner_id,
+		Habit_Name: 					reqData.Habit_Name,
+		Date_Created: 				reqData.Date_Created,
+		Comment: 							reqData.Comment,
+		Repeat_Count: 				reqData.Repeat_Count,
+		Target_Repeat_Count: 	reqData.Target_Repeat_Count,
+	}
+	row = database.DB.QueryRow(`
+		INSERT INTO
+		habits (owner_id, habit_name, date_created, comment, target_repeat_count, repeat_count)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING id`, 
+		owner_id,
+		reqData.Habit_Name,
+		reqData.Date_Created,
+		reqData.Comment,
+		reqData.Target_Repeat_Count,
+		reqData.Repeat_Count,
+	)
+	err = row.Scan(&habit.ID)
+	if err, ok := err.(*pq.Error); ok {
+		if err.Code.Name() == "foreign_key_violation" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": `Habit list: '`+reqData.Habit_Name+`' does not exist` ,
+			})
+		} else {
+			log.Println("Error: ", err)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": err.Message,
+			})
+		}
+	}
 
-// 	if err := database.DB.Raw(`SELECT * FROM habits
-// 			WHERE owner_id = ? AND date_created = ? AND habit_name = ?
-// 		`, owner_id, reqData.Date_Created, reqData.Habit_Name).
-// 		Scan(&oldHabit).Error; err != nil {
-// 		if ( !(errors.Is(err, gorm.ErrRecordNotFound)) ) {
-// 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 				"message": err.Error(),
-// 			})
-// 		}
-// 	}
-// 	// returning error if record exists
-// 	if (oldHabit != models.Habit{}) {
-// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 			"message": "Habit already exists",
-// 		})
-// 	}
-
-// 	//* saving the habit
-// 	habit := models.Habit {
-// 		Owner_ID: 						owner_id,
-// 		Habit_Name:						reqData.Habit_Name,
-// 		Date_Created: 				reqData.Date_Created,
-// 		Comment: 							reqData.Comment,
-// 		Target_Repeat_Count: 	reqData.Target_Repeat_Count,
-// 		Repeat_Count: 				reqData.Repeat_Count,
-// 	}
-// 	if err := database.DB.Create(&habit).Error; err != nil {
-// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-
-// 	log.Println("Successfully saved habbit")
-// 	return c.JSON(habit)
-// }
+	log.Println("Successfully saved habbit")
+	return c.JSON(habit)
+}
--- server/controllers/habitControllers/createHabitList.go
+++ server/controllers/habitControllers/createHabitList.go
@@ -1,84 +1,77 @@
 package controllers
 
-// import (
-// 	"errors"
-// 	"habit-tracker/database"
-// 	"habit-tracker/helpers"
-// 	"habit-tracker/middlewares"
-// 	"habit-tracker/models"
-// 	"log"
-// 	"strconv"
+import (
+	"habit-tracker/database"
+	"habit-tracker/helpers"
 
-// 	"github.com/gofiber/fiber/v2"
-// 	"github.com/golang-jwt/jwt/v4"
-// 	"gorm.io/gorm"
-// )
+	"habit-tracker/middlewares"
+	"habit-tracker/models"
+	"log"
+
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/lib/pq"
+	"github.com/golang-jwt/jwt/v4"
+)
 
 
-// func CreateHabitList(c *fiber.Ctx) error {
-// 	//* auth middleware
-// 	token := middlewares.AuthMiddleware(c)
-// 	if token == nil {
-// 		return c.JSON(fiber.Map{
-// 			"message": "Unauthenticated",
-// 		})
-// 	}
-// 	claims := token.Claims.(*jwt.RegisteredClaims)
+func CreateHabitList(c *fiber.Ctx) error {
+	//* auth middleware
+	token := middlewares.AuthMiddleware(c)
+	if token == nil {
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*jwt.RegisteredClaims)
 
-// 	u64, err := strconv.ParseUint(claims.Issuer, 10, 32)
-// 	if err != nil {
-// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	owner_id := uint(u64)
-	
-// 	//* data validation
-// 	reqData := new(ReqCreateHabitList)
-// 	if err := c.BodyParser(&reqData); err != nil {
-// 		log.Println("err: ", err)
-// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-// 	reqErrors := helpers.ValidateStruct(*reqData)
-// 	if reqErrors != nil {
-// 		return c.Status(fiber.StatusBadRequest).JSON(reqErrors)
-// 	}
+	u64, err := strconv.ParseUint(claims.Issuer, 10, 32)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	owner_id := uint(u64)
 
-// 	//* checking if it already exists
-// 	oldHabitList := models.HabitList{}
-// 	if err := database.DB.Raw(`
-// 			SELECT * FROM habit_lists WHERE owner_id = ? AND habit_name = ? LIMIT 1`,
-// 			owner_id, reqData.Habit_Name).
-// 		Scan(&oldHabitList).Error; err != nil {
-// 			if ( !(errors.Is(err, gorm.ErrRecordNotFound)) ) {
-// 				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 					"message": err.Error(),
-// 				})
-// 		}
-// 		log.Println(err)
-// 	}
-// 	// returning error if old habit list already exists
-// 	if (oldHabitList != models.HabitList{}) {
-// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 			"message": "Habitlist already exists",
-// 		})
-// 	}
+	//* data validation
+	reqData := new(ReqCreateHabitList)
+	if err := c.BodyParser(&reqData); err != nil {
+		log.Println("err: ", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	reqErrors := helpers.ValidateStruct(*reqData)
+	if reqErrors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(reqErrors)
+	}
+	//* saving the habitlist
+	habit := models.HabitList {
+		Owner_ID: 						owner_id,
+		Habit_Name:						reqData.Habit_Name,
+		Color: 								reqData.Color,
+		Default_Repeat_Count: reqData.Default_Repeat_Count,
+	}
+	row := database.DB.QueryRow(`
+		INSERT INTO
+		habit_lists (owner_id, habit_name, icon_url, color, default_repeat_count)
+		VALUES ($1, $2, $3, $4, $5) RETURNING id`, 
+		owner_id, reqData.Habit_Name, reqData.Icon_Url, reqData.Color, reqData.Default_Repeat_Count,
+	)
+	err = row.Scan(&habit.ID)
+	if err, ok := err.(*pq.Error); ok {
+		if err.Code.Name() == "unique_violation" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": `Habit list: '`+reqData.Habit_Name+`' already exists` ,
+			})
+		} else {
+			log.Println("Error: ", err, "Error code: ", err.Code.Name())
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": err.Message,
+			})
+		}
+	}
 
-// 	//* saving the habit
-// 	habit := models.HabitList {
-// 		Owner_ID: owner_id,
-// 		Habit_Name: reqData.Habit_Name,
-// 		Icon_Url: reqData.Icon_Url,
-// 		Color: reqData.Color,
-// 		Default_Repeat_Count: reqData.Default_Repeat_Count,
-// 	}
-// 	if err := database.DB.Create(&habit).Error; err != nil {
-// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 			"message": err.Error(),
-// 		})
-// 	}
-
-// 	return c.JSON(habit)
-// }
+	return c.JSON(habit)
+}
