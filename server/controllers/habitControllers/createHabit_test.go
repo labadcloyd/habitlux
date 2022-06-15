5023a1de1abea8dbfb7190ecd5a7344da069bcf4
cloyd <abadcloyd@gmail.com>
2022-06-15 20:23:10 +0800

    generated test templates for habitroutes

---
+++ server/controllers/habitControllers/createHabit_test.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestCreateHabit(t *testing.T) {
+	type args struct {
+		c *fiber.Ctx
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := CreateHabit(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("CreateHabit() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
